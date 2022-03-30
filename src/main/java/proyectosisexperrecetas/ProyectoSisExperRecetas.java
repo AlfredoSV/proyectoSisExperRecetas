225f758d0ff16b9779d960c23bf02ddd215e1fd4
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-30 10:56:06 -0600

    first commit

---
+++ src/main/java/proyectosisexperrecetas/ProyectoSisExperRecetas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package proyectosisexperrecetas;
+
+/**
+ *
+ * @author Alfredo
+ */
+public class ProyectoSisExperRecetas {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+        
+        Inicio inicio = new Inicio();
+        
+        
+        
+       
+        
+    }
+}
